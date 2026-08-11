ea58b060604c27b766e9318c786982521baaf0da
Levashov-VV <132905799+Levashov-VV@users.noreply.github.com>
2026-08-11 09:51:29 +0300

    Updates

---
+++ assets/pdfFonts-C4Cz6Ide.js
@@ -0,0 +1,1 @@
+import"./react-pdf.browser-CAJG3abf.js";import{n as e,t}from"./pdfFonts-D0uT76dz.js";export{t as PDF_FONT_FAMILY,e as registerPdfFonts};
--- assets/pdfFonts-DMNk7Oaj.js
+++
@@ -1,1 +0,0 @@
-import"./react-pdf.browser-BKa5Ph8L.js";import{n as e,t}from"./pdfFonts-BMWejNHX.js";export{t as PDF_FONT_FAMILY,e as registerPdfFonts};
